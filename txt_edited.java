e7456adeb1713d0b7a379390d3eac93d80b73bcb
ituitis20-doganumu20 <doganumu20@itu.edu.tr>
2024-03-08 17:44:16 +0300

    new

---
+++ txt_edited.java
@@ -0,0 +1,32 @@
+/**
+ * Bu bir JavaDoc yorum örneğidir.
+ * Ayrıca çoklu satırlıdır.
+ */
+
+// Bu bir tek satırlı yorum örneğidir.
+
+public class TestClass {
+
+    /*
+     * Bu bir çok satırlı yorum örneğidir.
+     * Satır 1
+     * Satır 2
+     */
+
+    // Bu bir tek satırlı yorum örneğidir.
+    public static void main(String[] args) {
+        // Bu da bir tek satırlı yorum örneğidir.
+
+        System.out.println("Merhaba, dünya!");  //buraya da bir yorum koydum
+
+        // Bu bir tek satırlı yorum örneğidir.
+    }
+
+    /*
+     * Bu bir çok satırlı yorum örneğidir.
+     * Satır 1
+     * Satır 2
+     */
+
+    // Bu bir tek satırlı yorum örneğidir.
+}
